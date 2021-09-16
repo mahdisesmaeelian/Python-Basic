83aa06f1ebb80225662c8edf9c6b3cd4a8e7e811
Mahdis Esmaeelian <88204357+mahdisesmaeelian@users.noreply.github.com>
2021-09-16 23:11:44 +0430

    Add files via upload

--- assignment-11/snake.py
+++ assignment-11/snake.py
@@ -19,18 +19,25 @@ class Snake(arcade.Sprite):
 
     def draw(self):
         arcade.draw_circle_filled(self.center_x,self.center_y,self.r,self.color)
-        self.snakebody_list.append([self.center_x,self.center_y])
         for i in self.snakebody_list:
             arcade.draw_circle_filled(i[0],i[1],self.r,self.color)
            
     def move(self):
-        self.center_x+=self.speed*self.change_x
-        self.center_y+=self.speed*self.change_y
+        for i in range(len(self.snakebody_list)-1, 0, -1):
+                self.snakebody_list[i][0] = self.snakebody_list[i-1][0]
+                self.snakebody_list[i][1] = self.snakebody_list[i-1][1]
+                
+        self.center_x += self.speed * self.change_x
+        self.center_y += self.speed * self.change_y
+        
+        if self.snakebody_list:
+            self.snakebody_list[0][0] += self.speed * self.change_x
+            self.snakebody_list[0][1] += self.speed * self.change_y
 
     def eat(self):
+        self.snakebody_list.append([self.center_x,self.center_y])
         self.score += 1   
         
-
 class Apple(arcade.Sprite):
     def __init__(self, w, h):
         arcade.Sprite.__init__(self)
@@ -45,6 +52,31 @@ class Apple(arcade.Sprite):
         arcade.draw_circle_filled(
             self.center_x, self.center_y, self.r, self.color)
 
+class Apple(arcade.Sprite):
+    def __init__(self, w, h):
+        super().__init__("apple.png")
+        self.width = 25
+        self.height = 30
+        self.center_x = random.randint(0, w)
+        self.center_y = random.randint(0, h)
+
+
+class Pear(arcade.Sprite):
+    def __init__(self, w, h):
+        super().__init__("pear.png")
+        self.width = 35
+        self.height = 35
+        self.center_x = random.randint(0, w)
+        self.center_y = random.randint(0, h)  
+
+class Poop(arcade.Sprite):
+    def __init__(self, w, h):
+        super().__init__("poop.png")
+        self.width = 35
+        self.height = 35
+        self.center_x = random.randint(0, w)
+        self.center_y = random.randint(0, h) 
+
 
 class Game(arcade.Window):
     def __init__(self):
@@ -52,11 +84,20 @@ class Game(arcade.Window):
         arcade.set_background_color(arcade.color.SAND)
         self.snake = Snake(700, 550)
         self.apple = Apple(700, 550)
+        self.pear = Pear(700,550)
+        self.poop = Poop(700,550)
 
     def on_draw(self):
         arcade.start_render()
-        self.apple.draw()
-        self.snake.draw()
+
+        if self.snake.score > -1 :
+            self.apple.draw()
+            self.snake.draw()
+            self.pear.draw()
+            self.poop.draw()
+            arcade.draw_text(f"Score: {self.snake.score}",570,20,arcade.color.WHITE,25)
+        else:
+            arcade.draw_text('Game Over', 160, 250, arcade.color.BLACK, width=400,font_size = 45, align='center')     
          
 
     def on_key_release(self, key, modifier):
@@ -76,11 +117,22 @@ class Game(arcade.Window):
     def on_update(self, delta_time: float):
 
         self.snake.move()
-
+        
         if arcade.check_for_collision(self.snake,self.apple):
             self.snake.eat()
             self.apple = Apple(700, 550)
-            print(self.snake.score)   
+            print(self.snake.score)  
+
+        if arcade.check_for_collision(self.snake,self.pear):
+            self.pear = Pear(700, 550)    
+            self.snake.score+=2
+
+        if arcade.check_for_collision(self.snake,self.poop):
+            self.poop = Poop(700, 550)    
+            self.snake.score-=1    
+
+        if (self.snake.center_x < 0) or (self.snake.center_x > 700) or (self.snake.center_y < 0) or (self.snake.center_y > 550) : 
+            self.snake.score = -1   
 
 game = Game()
 arcade.run()
