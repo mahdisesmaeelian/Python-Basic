10a8b244d3006df5fb3e54d46674794a624f14b6
Mahdis Esmaeelian <88204357+mahdisesmaeelian@users.noreply.github.com>
2021-09-22 11:28:19 +0330

    Add files via upload

--- assignment-14/platformer game/main.py
+++ assignment-14/platformer game/main.py
@@ -1,102 +1,119 @@
+import random
 import time
 import arcade
-from player1 import Player
-from enemy import Enemy
-from ground import Ground
+from arcade.key import X
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.texture = arcade.load_texture("img/20.png")
+
+        self.center_x = 100
+        self.center_y = 140
+        
+        self.width = 70
+        self.height = 85
+
+class Enemy(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        
+        self.center_x = x
+        self.center_y = y
+        self.speed = 5
+        self.change_x = -4
+
+        self.cactus = random.choice(['1','2','3','4'])
+
+        if self.cactus == '1':
+            self.texture = arcade.load_texture("img/6.png")
+        if self.cactus == '2':
+            self.texture = arcade.load_texture("img/7.png")
+        if self.cactus == '3':
+            self.texture = arcade.load_texture("img/8.png")
+        if self.cactus == '4':
+            self.texture = arcade.load_texture("img/9.png")
+
+
+class Ground(arcade.Sprite):
+    def __init__(self , x, y):
+        super().__init__()
+
+        self.texture = arcade.load_texture("img/15.png")
+
+        self.center_x = x
+        self.center_y = y
+
+        self.change_x = -4
+        self.change_y = 0   
 
 class Game(arcade.Window):
     def __init__(self):
         self.w = 800
         self.h = 500
-        self.gravity = 0.3
         super().__init__(self.w , self.h ,"My Game")
-        self.backeground_image = arcade.load_texture("img/background.png")
+        arcade.set_background_color(arcade.color.WHITE)
+        self.t1 = 2
+        self.gravity = 0.5
+        self.cactustime = 2
 
-        self.t1 = time.time()
         self.player = Player()
-        self.ground_list = arcade.SpriteList()
-        self.enemy_list = arcade.SpriteList()
-        
-        self.key = arcade.Sprite(":resources:images/items/keyYellow.png")
-        self.key.center_x=100
-        self.key.center_y=450
-        self.key.width = 50
-        self.key.height=50 
-        
-        self.lock = arcade.Sprite(":resources:images/tiles/lockYellow.png")
-        self.lock.center_x = 770
-        self.lock.center_y = 90
-        self.lock.width = 50
-        self.lock.height=50 
-
-        for i in range(0, 1000, 115):
-            ground = Ground(i ,7)
-            self.ground_list.append(ground)
-
-        for i in range(90,330,115):
-            ground = Ground(i ,240)   
-            self.ground_list.append(ground) 
-
-        for i in range(520,650,115):
-            ground = Ground(i ,190)   
-            self.ground_list.append(ground)     
-
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player,self.ground_list,self.gravity)
-
-        self.enemy_physics_engine_list = []
+        self.enemy_list = []
+        self.ground_list = []
+        self.my_physics_engine = []
 
     def on_draw(self):
-        arcade.start_render()    
-        arcade.draw_lrwh_rectangle_textured(0,0,self.w,self.h,self.backeground_image)
+        arcade.start_render() 
 
-        try:
-            self.key.draw()      
-        except:
-            pass    
+        for enemy in self.enemy_list:
+            enemy.draw()
 
-        self.lock.draw()
- 
-        self.player.draw()
         for ground in self.ground_list:
             ground.draw()
-        for enemy in self.enemy_list:
-            enemy.draw()    
 
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            self.player.change_x = -1 * self.player.speed
-        elif key == arcade.key.RIGHT:
-            self.player.change_x = 1 * self.player.speed
-        elif key == arcade.key.UP:
-            if self.physics_engine.can_jump():
-                self.player.change_y = 10
+        self.player.draw()
+        
+    
 
-    def on_key_release(self, key, modifiers):
-        self.player.change_x = 0           
-            
     def on_update(self, delta_time: float):
-        self.physics_engine.update()
-        self.player.update_animation()
-        for enemy in self.enemy_physics_engine_list:
-            enemy.update()
+     
+        self.player.update()
+        
+        for my in self.my_physics_engine:
+            my.update()
 
-        self.t2 = time.time()
 
-        if self.t2 - self.t1 > 5 :
-            new_enemy = Enemy()
-            self.enemy_list.append(new_enemy)
-            self.enemy_physics_engine_list.append(arcade.PhysicsEnginePlatformer(new_enemy,self.ground_list,self.gravity))
+        self.t2 = time.time() 
+        if self.t2 - self.t1 > 2:
+            new_ground = Ground(self.w , self.h//5)
+            self.ground_list.append(new_ground)
+            self.my_physics_engine.append(arcade.PhysicsEnginePlatformer(self.player,self.ground_list,gravity_constant= self.gravity))
             self.t1 = time.time()
-        try:
-            if arcade.check_for_collision(self.player,self.key):
-                self.player.pocket.append(self.key)   
-                del self.key   
-                print(len(self.player.pocket)) 
-        except:
-            pass  
 
-        if arcade.check_for_collision(self.player,self.lock) and len(self.player.pocket)==1 :
-            self.lock.texture = arcade.load_texture(":resources:images/items/star.png")            
+        if time.time() - self.t1 > self.cactustime:
+            self.enemy_list.append(Enemy(self.w, self.h//3.8))
+            self.t1 = time.time()
+ 
+        for enemy in self.enemy_list:
+            enemy.update()
+            if enemy.center_x < 40:
+                self.enemy_list.remove(enemy)
+
+        for ground in self.ground_list:
+            ground.update()
+            if ground.center_x < 1:
+                self.ground_list.remove(ground)
+
+    # def on_key_press(self, key, modifiers):
+    #     if key == arcade.key.UP:
+    #         if self.my_physics_engine.can_jump():
+    #             self.player.change_y = 1
+
+    # def on_key_release(self, key, modifiers):
+    #     self.player.change_y = 0
+                    
+     
 
 game = Game()
-arcade.run()        
+arcade.run()         
