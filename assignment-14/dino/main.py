70984d7c4d2f48ec7dafcfadf27e36359841960e
Mahdis Esmaeelian <88204357+mahdisesmaeelian@users.noreply.github.com>
2021-09-24 15:49:34 +0330

    Add files via upload

--- assignment-14/dino/main.py
+++ assignment-14/dino/main.py
@@ -16,14 +16,16 @@ class Game(arcade.Window):
         self.high_score = self.score
         arcade.set_background_color(arcade.color.WHITE)
         self.tcactus = time.time()
-        self.birdtime = 5
         self.tground = 1
+        self.birdtime = 5
         self.gravity = 0.4
         self.cactustime = 2
         self.jump_sound = (arcade.load_sound(":resources:sounds/rockHit2.wav"))
         self.die_sound = (arcade.load_sound("sprites_die.wav"))
         self.starttime = time.time()
+        self.startdinotime = time.time()
         self.flag = 0
+        self.flag1 = 0
         self.game_over = False
 
         self.player = Player()
@@ -56,17 +58,16 @@ class Game(arcade.Window):
         for ground in self.ground_list:
             ground.draw()
 
-        self.player.draw()         
+        self.player.draw()     
 
-    def on_update(self, delta_time: float):
-   
+        
+    def on_update(self, delta_time: float):   
+
         if self.game_over == False:
             self.score += 1
             self.player.update()
             self.physics_engine.update()
-            print(self.high_score)
-
-            
+                       
             if self.score > 1000:
                 for bird in self.bird_list:
                     bird.update()
@@ -85,24 +86,33 @@ class Game(arcade.Window):
                 if ground.center_x < 510:
                     self.ground_list.remove(ground)
 
+        self.dinowalk = time.time()
+            
+        if self.dinowalk - self.startdinotime > 0.2 and self.flag1 == 0:
+            self.startdinotime = time.time()
+            self.player.texture = arcade.load_texture("img/19.png")
+            self.flag1 = 1
+
+        if self.dinowalk - self.startdinotime > 0.2 and self.flag1 == 1:
+            self.player.texture = arcade.load_texture("img/20.png")
+            self.startdinotime = time.time()
+            self.flag1 = 0 
             
             self.bgcolortime = time.time()
             
             if self.bgcolortime - self.starttime > 15 and self.flag == 0:
                 self.starttime = time.time()
-                arcade.draw_text(f"{self.score}",650,450,arcade.color.WHITE,25)
                 arcade.set_background_color(arcade.color.BLACK)
                 self.flag = 1
 
             if self.bgcolortime - self.starttime > 15 and self.flag == 1:
                 arcade.set_background_color(arcade.color.WHITE)
-                arcade.draw_text(f"{self.score}",650,450,arcade.color.BLACK,25)
                 self.starttime = time.time()
                 self.flag = 0           
 
             self.t2 = time.time() 
-
-            if time.time() - self.birdtime > 10:
+            
+            if time.time() - self.birdtime > 15:
                 self.bird_list.append(Bird(self.w, self.h//2.2))
                 self.birdtime = time.time()
 
@@ -114,33 +124,58 @@ class Game(arcade.Window):
                 self.enemy_list.append(Enemy(self.w, self.h//3.8))
                 self.tcactus = time.time()
     
+            for bird in self.bird_list:
+                if  arcade.check_for_collision(self.player ,bird) :
+                    arcade.play_sound(self.die_sound)
+                    self.game_over = True
+                    self.tcactus = time.time()
+                    self.new_score = self.score
+                    if self.new_score >= self.high_score :
+                        self.high_score = self.score
+                    arcade.set_background_color(arcade.color.WHITE)
+                    self.score = 0
+                    self.birdtime = 5
+                    self.tground = 1
+                    self.gravity = 0.4
+                    self.cactustime = 2
+                    self.starttime = time.time()
+                    self.flag = 0
+                    self.flag1 = 0
+                    self.player = Player()
+                    self.enemy_list = arcade.SpriteList()
+                    self.ground_list = arcade.SpriteList()
+                    self.bird_list = arcade.SpriteList()
+                    self.physics_engine = arcade.PhysicsEnginePlatformer(self.player,self.ground_list,self.gravity)
+                                    
+                    
             for cactus in self.enemy_list:
-                for bird in self.bird_list:
-                    if arcade.check_for_collision(self.player ,cactus) or arcade.check_for_collision(self.player ,bird) :
-                        arcade.play_sound(self.die_sound)
-                        self.game_over = True
-                        self.tcactus = time.time()
-                        self.new_score = self.score
-                        if self.new_score >= self.high_score :
-                            self.high_score = self.score
-                        arcade.set_background_color(arcade.color.WHITE)
-                        self.score = 0
-                        self.birdtime = 5
-                        self.tground = 1
-                        self.gravity = 0.4
-                        self.cactustime = 2
-                        self.starttime = time.time()
-                        self.flag = 0
-                        self.player = Player()
-                        self.enemy_list = arcade.SpriteList()
-                        self.ground_list = arcade.SpriteList()
-                        self.bird_list = arcade.SpriteList()
-                        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player,self.ground_list,self.gravity)
-                        
+                if arcade.check_for_collision(self.player ,cactus):
+                    arcade.play_sound(self.die_sound)
+                    self.game_over = True
+                    self.tcactus = time.time()
+                    self.new_score = self.score
+                    if self.new_score >= self.high_score :
+                        self.high_score = self.score
+                    arcade.set_background_color(arcade.color.WHITE)
+                    self.score = 0
+                    self.birdtime = 5
+                    self.tground = 1
+                    self.gravity = 0.4
+                    self.cactustime = 2
+                    self.starttime = time.time()
+                    self.flag = 0
+                    self.flag1 = 0
+                    self.player = Player()
+                    self.enemy_list = arcade.SpriteList()
+                    self.ground_list = arcade.SpriteList()
+                    self.bird_list = arcade.SpriteList()
+                    self.physics_engine = arcade.PhysicsEnginePlatformer(self.player,self.ground_list,self.gravity)
+                       
     def on_key_press(self, key, modifiers):
 
         if key == arcade.key.UP:
             if self.physics_engine.can_jump():
+                self.player.texture = arcade.load_texture("img/19.png")
                 arcade.play_sound(self.jump_sound)
                 self.player.change_y = 10
 
