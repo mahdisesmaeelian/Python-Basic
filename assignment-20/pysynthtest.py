668e097c2d0a261b7587d3cf177c73009e3bc467
Mahdis Esmaeelian <88204357+mahdisesmaeelian@users.noreply.github.com>
2021-10-25 16:49:16 +0330

    Update pysynthtest.py

--- assignment-20/pysynthtest.py
+++ assignment-20/pysynthtest.py
@@ -1,19 +1,9 @@
- import pysynth_b
+import pysynth_b
+import play_wav
 
-def write(self, melody,title,times=None):
-     abc_notation = []
-     for i in range(len(melody)):
-         if isinstance(melody[i],tuple):
-             note = melody[i][0]
-             note_time = melody[i][1]
-         else:
-             note = melody[i]
-             if times:
-                 note_time = times[i]
-                 if note_time == 0:
-                     note_time = 4
-             else:
-                 note_time = 4
-         abc_notation.append((self.number_to_abc(note),note_time))
-     print("ABC to write:",abc_notation)
-     pysynth_b.make_wav(abc_notation,fn=title)
+# (('g', 4), ('g', 4), ('c6', 2),('g',4), ('g', 4), ('c6', 2))
+my_string = eval(input())
+
+pysynth_b.make_wav(my_string, fn = "test.wav")
+
+play_wav.Sound().playFile('test.wav')
