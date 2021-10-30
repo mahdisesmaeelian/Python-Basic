a16b16cbc588e5d433ebe70732f34f342b19455b
Mahdis Esmaeelian <88204357+mahdisesmaeelian@users.noreply.github.com>
2021-10-30 17:56:51 +0330

    Add files via upload

---
+++ assignment-21/Checkered/Checkered without input image.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+img = np.zeros((800, 800, 1), dtype = "uint8")
+
+blacksquare = img[0:100, 0:100] = 255
+
+for i in range(0, 800, 200):
+    for j in range(0, 800, 200):
+        img[i:i+100, j:j+100] = blacksquare
+
+for i in range(100, 800, 200):
+    for j in range(100, 800, 200):
+        img[i:i+100, j:j+100] = blacksquare
+
+cv2.imwrite('Output.jpg', img)
+
+cv2.imshow('photo', img)
+cv2.waitKey()
